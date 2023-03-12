a8f5724ca04192f0466dad27a60f0c4752899c21
vivek singh <cu.vivek.1557@gmail.com>
2023-03-12 10:34:49 +0530

    adding message input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Input() {
   return (
-    <div className='input'>Input</div>
+    <div className='input'>
+      <input type='text' placeholder='enter something'/>
+      <div className="send">
+      <input type='file' id='file' style={{display:'none'}}/>
+      <span><label htmlFor='file'> <i class='bx bx-paperclip bx-sm'></i></label></span>
+      <span><i class='bx bxs-send bx-sm' style={{color:'#183a1d'}}  ></i></span>
+      </div>
+    </div>
   )
 }
