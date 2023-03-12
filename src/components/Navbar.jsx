a24e74fad69306449a786993d49f23c123a0cf19
Your Name <you@example.com>
2023-03-12 15:15:22 +0530

    adding some style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <span className='logo'>Logo</span>
+      <span className='logo'><i class='bx bxl-snapchat bx-sm' style={{color:'#ffffff'}}  ></i></span>
       <div className='user'>
           <img src='https://files.codingninjas.com/profile236766498c77d6f39b7da1c4e0c22ffee73e0cf.png' alt='img'/>
           <span>Hola</span>
-          <button>Logout</button>
+          <button><span>Logout</span> <i class='bx bx-log-out bx-sm bx-rotate-180'  ></i></button>
       </div>
     </div>
   )
