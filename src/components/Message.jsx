98e0087cd859fbd0983736441765e6b3414d213c
vivek singh <cu.vivek.1557@gmail.com>
2023-03-12 11:39:37 +0530

    complete message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Messange() {
   return (
-    <div>Messange</div>
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src='https://files.codingninjas.com/profile236766498c77d6f39b7da1c4e0c22ffee73e0cf.png' alt='avtar'/>
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src='https://files.codingninjas.com/profile236766498c77d6f39b7da1c4e0c22ffee73e0cf.png' alt='avtar'/>
+      </div>
+    </div>
   )
 }
